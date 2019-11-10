53853196aea23d756b78c3557c536e547f1297ec
sh183694@gmail.com <sh183694@gamil.com>
2019-11-10 18:08:11 +0900

    ADD styled-components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 
 function App() {
-  return <div>hello react</div>;
+  return (
+    <Container>
+      <Button>Hello</Button>
+      <Button danger>Hello</Button>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: pink;
+`;
+
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  -webkit-appearance: none;
+  cursor: pointer;
+  &:active,
+  &:focus {
+    outline: none;
+  }
+  background-color: ${props => (props.danger ? "#e74c3c" : "#2ecc71")};
+`;
 
 export default App;
